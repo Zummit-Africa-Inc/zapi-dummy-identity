8a200302b034b9ad08ff60442bd774a31b80cd09
alahirajeffrey <alahirajeffrey@gmail.com>
2022-07-19 06:12:51 +0100

    added api documentation

--- src/user/dto/change-password.dto.ts
+++ src/user/dto/change-password.dto.ts
@@ -1,10 +1,17 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsString, Matches, MaxLength, MinLength } from "class-validator";
 import { Match } from "src/common/decorators/password-match.decorator";
 
 export class ChangePasswordDto{
 
     @IsString()
-    @IsNotEmpty({message: 'password cannot be empty'})
+    @IsNotEmpty({message: 'Please input your current password'})
+    @ApiProperty()
+    oldPassword: string
+
+    @IsString()
+    @ApiProperty()
+    @IsNotEmpty({message: 'Please input your new password'})
     @MinLength(8, 
         {message: 'Password is too short. Minimal length is $constraint1 characters, but actual is $value'}
     )
@@ -14,16 +21,16 @@ export class ChangePasswordDto{
     @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, 
         {message: 'password must contain the following: a capital letter, a number and a special character'}
     )
-    password: string;
+    newPassword: string;
 
     @IsString()
     @IsNotEmpty({message: 'kindly confirm password'})
-    @MinLength(8)
-    @MaxLength(20)
-    @Match('password')
-    passwordConfirm: string;
+    @Match('newPassword')
+    @ApiProperty()
+    newPasswordConfirm: string;
 
     @IsString()
+    @ApiProperty()
     @IsNotEmpty({message: 'kindly input access token'})
     accessToken: string
-}
+ }
