0dcabade52149949a1d6550a3a06aff6926d5cd2
alahirajeffrey <alahirajeffrey@gmail.com>
2022-07-21 22:26:20 +0100

    removed validations

--- src/user/dto/change-password.dto.ts
+++ src/user/dto/change-password.dto.ts
@@ -12,15 +12,6 @@ export class ChangePasswordDto{
     @IsString()
     @ApiProperty()
     @IsNotEmpty({message: 'Please input your new password'})
-    @MinLength(8, 
-        {message: 'Password is too short. Minimal length is $constraint1 characters, but actual is $value'}
-    )
-    @MaxLength(20, 
-        { message: 'password is too long. Maximal length is $constraint1 characters, but actual is $value'}
-    )
-    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, 
-        {message: 'password must contain the following: a capital letter, a number and a special character'}
-    )
     newPassword: string;
 
     @IsString()
